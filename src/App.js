daf95dc94c118c5274bfad54f295b400a3e4b175
Malekk0 <44036411+Malekk0@users.noreply.github.com>
2019-03-20 20:27:24 +0200

    добавлен Body

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
 import logo from "./logo.svg";
+import body from "./components/Body/body";
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Body from "./components/Body/body";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header logo={logo} header="НАЗВАНИЕ САЙТА" />
+        <Body />
         <Footer />
       </div>
     );
